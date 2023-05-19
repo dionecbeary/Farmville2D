3e2c38bf2f508017f1bba72eb4f97a018b2346c7
DioneCheung <chee-tung-dione_cheung25@milton.edu>
2023-05-19 13:09:24 -0400

    Continued ArrayList Practice Problems

--- src/MyFarmArrayList.java
+++ src/MyFarmArrayList.java
@@ -17,6 +17,11 @@ public class MyFarmArrayList {
         row.add(new Plot());
         System.out.println(row.size());
         totalPlants();
+        printPlantNames();
+        addSunflower();
+        printPlantNames();
+        addCorn();
+        printPlantNames();
     }
 
     public void totalPlants() {
@@ -27,5 +32,34 @@ public class MyFarmArrayList {
         }
         System.out.println("Total plants in my farm is "+total);
     }
+    public void printPlantNames() {
+        // print the name of each plant in each row and column
+        // row: 0 col: 0 corn
+        for(int k = 0; k < row.size(); k++){
+            System.out.print(k + ": "+row.get(k).plantName + "\t");
+        }
+        System.out.println();
+    }
+    public void addSunflower() {
+        // add a lettuce plot to the end of the row with 42 plants that need water
+        // call printPlantNames() method before and after your addition to make sure it worked
+        Plot sunflower = new Plot();
+        sunflower.plantName = "sunflower";
+        sunflower.numberOfPlant = 42;
+        sunflower.needsWater = true;
+        sunflower.printPlot();
+        row.add(sunflower);
+    }
+    public void addCorn() {
+        // at index 3, add a new tulip plot with 70 plants that do not need water
+        // call printPlantNames() method before and after your addition to make sure it worked
+        Plot corn = new Plot();
+        corn.plantName = "corn";
+        corn.numberOfPlant = 70;
+        corn.needsWater = false;
+        corn.printPlot();
+        row.add(3,corn);
+    }
+
 
 }
